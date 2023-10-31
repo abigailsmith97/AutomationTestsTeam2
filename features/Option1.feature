9b1e4162126b26c5c9858bd96ea5f170c7187d44
Louis Ware <louisware@Soliriun197sMBP.localdomain>
2023-10-31 15:33:34 +0000

    Implemented test to verify user can remove items from cart

--- features/Option1.feature
+++ features/Option1.feature
@@ -2,3 +2,7 @@ Feature: Green Kart features
 Scenario: Homepage is visible
   Given I navigate to the Green Kart homepage 
   Then I should see the Green Kart homepage 
+
+Scenario: Item is in cart
+  Given I add item to the cart
+  Then I should be able to remove it
--- features/step_definitions/option_1_steps.rb
+++ features/step_definitions/option_1_steps.rb
@@ -4,7 +4,24 @@ Given('I navigate to the Green Kart homepage') do
   sleep 1 # The sleeps are added to slow down the automation to show it working
 end
 
-
 Then('I should see the Green Kart homepage') do
   pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('I add item to the cart') do
+  first(:button, 'ADD TO CART').click # <- Click add to cart
+  
+  expect(page).to have_selector('.cart-info', text:'1') # <- Verify 1 in cart
+
+end
+
+
+Then('I should be able to remove it') do
+
+  find(:xpath, "//img[@alt='Cart']").click # <- Click shopping cart icon
+
+  find(:xpath, "//div[@class='cart-preview active']//div//div//a[@class='product-remove'][normalize-space()='×']").click # <- Click x to remove item
+
+  expect(page).to have_selector('.cart-info', text:'0')  # <- Verify nothing in cart
+
 end
