8d3bc2a8df9c86cbd2f53d6a0f705b759a948bec
Lindsay Ezeagu <lindsayezeagu@Soliriun195sMBP.localdomain>
2023-10-31 15:56:04 +0000

    Added test to verify that a user can search for items

--- features/Option1.feature
+++ features/Option1.feature
@@ -1,4 +1,9 @@
 Feature: Green Kart features
 Scenario: Homepage is visible
   Given I navigate to the Green Kart homepage 
-  Then I should see the Green Kart homepage 
+  Then I should see the Green Kart homepage
+
+Scenario: Search for Brocolli
+  Given I navigate to the Green Kart homepage 
+  And I search for Brocolli 
+  Then I see the Brocolli on the page
--- features/step_definitions/option_1_steps.rb
+++ features/step_definitions/option_1_steps.rb
@@ -8,3 +8,28 @@ end
 Then('I should see the Green Kart homepage') do
   pending # Write code here that turns the phrase above into concrete actions
 end
+
+# When("I will search for {Brocolli}") do |searchText|
+#   fill_in "Brocolli", with => searchText
+#   sleep 1 
+# end
+
+# When("I click search") do 
+#   find('Brocolli').click
+#   sleep 1
+# end
+
+# Then("I should use {Brocolli}") do |expectedText|
+#   page.should have_content(expectedText)
+#   sleep 1
+# end
+
+Given('I search for Brocolli') do
+  #fill_in 'search', :with => "Brocolli"
+  find(:xpath, "//input[@placeholder='Search for Vegetables and Fruits']").set('Brocolli')
+  sleep 2
+end
+
+Then('I see the Brocolli on the page') do 
+  expect(page).to have_content('Brocolli')
+end
