283425182a0600e43dce351d5d1a66ad614ecdc5
SoliriusAdmin129 <soliriusadmin129@Soliriun129sMBP.localdomain>
2023-10-31 15:37:31 +0000

    first round of tests

--- features/Option2.feature
+++ features/Option2.feature
@@ -2,3 +2,8 @@ Feature: Para Bank features
 Scenario: Homepage is visible
   Given I navigate to the Para Bank homepage 
   Then I should see the Para Bank homepage 
+
+
+Scenario: Bill Pay Page
+  Given I navigate to the Para Bank homepage
+  Then I should 
--- features/step_definitions/option_1_steps.rb
+++ features/step_definitions/option_1_steps.rb
@@ -6,5 +6,29 @@ end
 
 
 Then('I should see the Green Kart homepage') do
-  pending # Write code here that turns the phrase above into concrete actions
+  page.has_title?('GreenKart - veg and fruits kart')
 end
+
+Given('I navigate to the homepage') do
+  visit 'https://rahulshettyacademy.com/seleniumPractise/#/'
+end
+
+When('I click on the checkout bag') do
+  click_on('#root > div > header > div > div.cart > a.cart-icon > img')
+end
+
+Then('I should see a summary list of my items') do
+  page.has_selector?('#root > div > header > div > div.cart > div.cart-preview.active')
+end
+
+When('I add items to the checkout bag') do
+  page.all(:button, 'ADD TO CART').click.each do |item|
+    puts item
+end
+  all(:button, 'ADD TO CART')[0].click
+end
+
+Then('I should see a total cost of the items') do
+  page.has_selector?('#root > div > header > div > div.cart > div.cart-info')
+end
+
